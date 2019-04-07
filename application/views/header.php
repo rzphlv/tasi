e6daccea84dfecec015f616d5ccd7afc05c51273
Reza Pahlevi <32995725+rzphlv@users.noreply.github.com>
2019-04-07 15:35:34 +0700

    .

--- application/views/header.php
+++ application/views/header.php
@@ -41,10 +41,10 @@
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar default-layout col-lg-12 col-12 p-0 ">
       <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
-        <a class="navbar-brand brand-logo" href="index.html">
+        <a class="navbar-brand brand-logo">
           <img src="<?php base_url() ?>images/iteras1.png" alt="logo" />
         </a>
-        <a class="navbar-brand brand-logo-mini" href="index.html">
+        <a class="navbar-brand brand-logo-mini">
           <img src="<?php base_url() ?>images/logo-mini.svg" alt="logo" />
         </a>
       </div>
