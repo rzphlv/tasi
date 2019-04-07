8fddc1f96cea36083eaf69cbc5281cf0c5e3a54a
muttoharoh <muttoharoh10@gmail.com>
2019-04-07 15:36:10 +0700

    .

--- application/views/referensi.php
+++ application/views/referensi.php
@@ -3,10 +3,189 @@
         <div class="regis">
 
         <h3><strong>Geoteknik</strong></h3>
+                <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+
+
+                        <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+                        <br>
+                        <br>
         <h3><strong>Hidrologi</strong></h3>
+
+                <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+
+
+                        <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+                        <br>
+                        <br>
+
         <h3><strong>Struktur</strong></h3>
+
+                <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+
+
+                        <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+                        <br>
+                        <br>
+
+
         <h3><strong>Transportasi</strong></h3>
+
+                <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+
+
+                        <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+                        <br>
+                        <br>
+
+
         <h3><strong>Manajemen</strong></h3>
+
+                <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+
+
+                        <br>
+                
+                <img class="ep_doc_icon" alt="[img]" src="http://digilib.unila.ac.id/7591/2.hassmallThumbnailVersion/2.Abstract.pdf" border="0"></a><div id="doc_preview_608621" class="ep_preview" style="display: none; z-index: 1000; top: 343px; left: 806.75px;"><table><tbody><tr><td><img class="ep_preview_image" alt="" src="http://digilib.unila.ac.id/7591/2.haspreviewThumbnailVersion/2.Abstract.pdf" border="0"><div class="ep_preview_title">Preview</div></td></tr></tbody></table></div></td>
+                    <td valign="top">
+                        <br>
+                                    <a href="http://digilib.unila.ac.id/7591/2/2.Abstract.pdf" class="ep_document_link">Download (19Kb)</a>
+                                    
+                                    | <a href="http://digilib.unila.ac.id/7591/2.haslightboxThumbnailVersion/2.Abstract.pdf" rel="lightbox[docs] nofollow" title="
+
+                        <!-- document citation -->
+                        <span class=&quot;ep_document_citation&quot; xmlns=&quot;http://www.w3.org/1999/xhtml&quot; xmlns:cite=&quot;http://eprints.org/ep3/citation&quot; xmlns:epc=&quot;http://eprints.org/ep3/control&quot;>
+                        <a class=&quot;ep_document_link&quot; href=&quot;http://digilib.unila.ac.id/7591/2/2.Abstract.pdf&quot;><span class=&quot;document_format&quot;>Text</span></a>
+                        <br /><span class=&quot;document_filename&quot;>2.Abstract.pdf</span>
+                        </span>
+                        ">Preview</a>
+                        <br>
+                        <br>
 
         </div>
     </div>
