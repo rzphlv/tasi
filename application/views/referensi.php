7a024c11f8fd442287e4faf87d62e40c9ead48ec
muttoharoh <muttoharoh10@gmail.com>
2019-04-07 15:02:07 +0700

    .

---
+++ application/views/referensi.php
@@ -0,0 +1,13 @@
+<div class="col-md-10">
+    <div class="col-md-9">
+        <div class="regis">
+
+        <h3><strong>Geoteknik</strong></h3>
+        <h3><strong>Hidrologi</strong></h3>
+        <h3><strong>Struktur</strong></h3>
+        <h3><strong>Transportasi</strong></h3>
+        <h3><strong>Manajemen</strong></h3>
+
+        </div>
+    </div>
+</div>
