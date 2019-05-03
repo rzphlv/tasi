0e2b66ce664f31ce9c47400bf59bfccef6e432d5
Muhammad Julian <muhammad.14116165@student.itera.ac.id>
2019-05-04 05:19:02 +0700

    login register controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,6 +5,57 @@ class Login extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('mahasiswa/login');
+		$email = $this->session->userdata('Email');
+		$options = array(
+			'img_path'=>'./captcha/', #folder captcha yg sudah dibuat tadi
+			'img_url'=>base_url('captcha'), #ini arahnya juga ke folder captcha
+			'img_width'=>'150', #lebar image captcha
+			'img_height'=>'45', #tinggi image captcha
+			'expiration'=>7200, #waktu expired
+			'word_length' => 4,
+			'pool' => '0123456789', #tipe captcha (angka/huruf, atau kombinasi dari keduanya)
+			# atur warna captcha-nya di sini ya.. gunakan kode RGB
+			'colors' => array(
+				'background' => array(242, 242, 242),
+				'border' => array(255, 255, 255),
+				'text' => array(0, 0, 0),
+				'grid' => array(255, 40, 40))
+		);
+
+		$cap = create_captcha($options);
+		$data['image'] = $cap['image'];
+		$this->session->set_userdata('mycaptcha', $cap['word']);
+		$data['word'] = $this->session->userdata('mycaptcha');
+		$data['title'] = "MtaGeo | Masuk";
+		if(!empty($email))
+		{
+		} else {
+			$this->load->view('mahasiswa/login', $data);
+		}
+
+	}
+	public function getlogin()
+	{
+		$captcha = $this->input->post('captcha_code'); #mengambil value inputan pengguna
+		$word = $this->session->userdata('mycaptcha'); #mengambil value captcha
+		$u = $this->input->post('email');
+		$p = $this->input->post('password');
+		if (isset($captcha)) { #cek variabel $captcha kosong/tidak
+		   if (strtoupper($captcha)==strtoupper($word)) { #proses pencocokan captcha
+		        #terserah kalian mau diisi apa di sini
+										$this->load->model('Model_Login');
+										$this->Model_Login->getlogin($u,$p);
+		   }
+					else {
+						$this->session->set_flashdata('info',
+								'<div class="alert alert-danger alert-dismissible">
+																								<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+																								<h4><i class="icon fa fa-ban"></i> Alert!</h4>
+																								Captcha Salah
+																						</div>');
+						redirect('login');
+					}
+		 }
 	}
 }
+
--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -7,4 +7,65 @@ class Register extends CI_Controller {
 	{
 		$this->load->view('mahasiswa/register');
 	}
+
+	public function simpan()
+	{
+		$key = $this->input->post('email');
+		$email = $this->input->post('email');
+		$data['email']	= $this->input->post('email');
+		$saltid   = md5($this->input->post('email'));
+		$data['nama']	= $this->input->post('nama');
+		$data['password']	= md5($this->input->post('password'));
+		$this->load->model('Model_Register');
+		$query = $this->Model_Register->getdata($key);
+		$cektera=$this->Model_Resgister->cekitera($key);
+		$cek1=$this->Model_Register->checkEmail($key);
+		$cek2=$this->Model_Register->cekmail($key);
+
+		if($cektera == false) {
+			echo "<script>window.alert('Gunakan Email ITERA')</script>";
+			echo "<meta http-equiv='refresh' content='0;url=http://localhost/tasi/register'>";
+		}
+		else {
+			if($cek1 == false)
+			{
+				echo "<script>window.alert('Email anda invalid')</script>";
+				echo "<meta http-equiv='refresh' content='0;url=http://localhost/tasi/register'>";
+			}
+			else
+			{
+				if ($cek2 == false) {
+					echo "<script>window.alert('Email Sudah Terdaftar')</script>";
+					echo "<meta http-equiv='refresh' content='0;url=http://localhost/tasi/register'>";
+				}
+				else
+				{
+					$this->sendemail($email, $saltid);
+					$this->model_daftar->getinsert($data);
+					echo "<meta http-equiv='refresh' content='0;url=http://localhost/tasi/'>";
+				}
+			}
+		}
+	}
+
+	function sendemail($email,$saltid){
+		// configure the email setting
+		$config['protocol'] = 'smtp';
+		$config['smtp_host'] = 'ssl://smtp.googlemail.com'; //smtp host name
+		$config['smtp_port'] = 465; //smtp port number
+		$config['smtp_user'] = 'emailsimta@gmail.com';
+		$config['smtp_pass'] = 'emaildummy'; //$from_email password
+		$config['mailtype'] = 'html';
+		$config['charset'] = 'iso-8859-1';
+		$config['wordwrap'] = TRUE;
+        $config['newline'] = "\r\n"; //use double quotes
+        $this->email->initialize($config);
+        $url = base_url()."daftar/confirmation/".$saltid;
+        $this->email->from('emailsimta@gmail.com', 'VERIFIKASI PENDAFTARAN');
+        $this->email->to($email);
+        $this->email->subject('Verifikasi Email');
+        $message = "<html><head><head></head><body><p>Hi,</p><p>Terimakasih telah mendaftar.</p><p>ikuti tautan berikut untuk konfirmasi email.</p><a href=".$url."><b>VERIFIKASI</b></a><br/><p>Hormat,</p><p>Administrator SITA</p></body></html>";
+        $this->email->message($message);
+        return $this->email->send();
+    }
 }
